ace4732269081a0d01c1bf2cbe63daf5cf8c6e8b
Linus Östberg <linus@talavis.eu>
2016-10-14 14:56:01 +0200

    Simple run time testing of the different searching algorithms

---
+++ test_sort.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''
+Testing different sorting algorithms
+'''
+
+import timeit
+
+for i in (10, 10**2, 10**3, 10**4):
+    print('Sort {} random numbers:'.format(i))
+    setup = '''
+import random
+import insertion_sort as ins_sort
+import merge_sort as mer_sort
+import selection_sort as sel_sort
+
+values = list(range({}))
+random.shuffle(values)
+'''.format(i)
+
+    run = timeit.timeit('ins_sort.insertion_sort(values)', setup=setup, number=1)
+    print('Insertion sort: {}'.format(run))
+    run = timeit.timeit('sel_sort.selection_sort(values)', setup=setup, number=1)
+    print('Selection sort: {}'.format(run))
+    run = timeit.timeit('mer_sort.merge_sort(values)', setup=setup, number=1)
+    print('Merge sort: {}'.format(run))
+
