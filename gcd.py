af29b76809134ff86ac837d12160fb5a79be5f95
Linus Östberg <linus@talavis.eu>
2017-09-13 22:12:36 +0200

    Recursive GCD

---
+++ gcd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Recursive identification of GCD
+'''
+
+def gcdRecur(a, b):
+    '''
+    a, b: positive integers    
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    if b == 0:
+        return a
+    return gcdRecur(b, a % b)
